cd164385a97a280e99d278cdf30282da8bd4ddac
CastleJP <60377000+CastleJP@users.noreply.github.com>
2020-08-10 17:13:47 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Discord;
+namespace bot
+{
+    class Program
+    {
+        public static DiscordSocketClient client;
+        public static CommandService commands;
+        public static IServiceProvider services;
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+        public async Task MainAsync()
+        {
+            client = new DiscordSocketClient();
+            commands = new CommandService();
+            services = new ServiceCollection().BuildServiceProvider();
+            client.MessageReceived += CommandRecieved;
+            client.Log += Log;
+            string token = "tokenを入れる場所";
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+            await Task.Delay(-1);
+        }
+        private async Task CommandRecieved(SocketMessage messageParam)
+        {
+            var message = messageParam as SocketUserMessage;
+            Console.WriteLine("{0} {1}:{2}", message.Channel.Name, message.Author.Username, message);
+            if (message == null) { return; }
+            if (message.Author.IsBot) { return; }
+            int argPos = 0;
+            if (!(message.HasCharPrefix('.', ref argPos) || message.HasMentionPrefix(client.CurrentUser, ref argPos))) { return; }
+            var context = new CommandContext(client, message);
+            var result = await commands.ExecuteAsync(context, argPos, services);
+            if (!result.IsSuccess) { await context.Channel.SendMessageAsync(result.ErrorReason); }
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Roulette1.cs
@@ -0,0 +1,65 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace bot
+{
+    public class Messages : ModuleBase
+    {
+        private readonly Dictionary<int, string> rule = new Dictionary<int, string>()
+        {
+            {0, "ルーレット"},
+        };
+        private readonly Dictionary<int, string> stage = new Dictionary<int, string>()
+        {
+            {0,   "1"},
+            {1 ,  "2"},
+            {2 ,  "3"},
+            {3 ,  "4"},
+            {4 ,  "5"},
+            {5 ,  "6"},
+            {6 ,  "7"},
+            {7 ,  "8"},
+            {8 ,  "9"},
+            {9 ,  "10"},
+            {10,  "11"},
+            {11,  "12"},
+            {12,  "13"},
+            {13,  "14"},
+            {14,  "15"},
+            {15,  "16"},
+            {16,  "17"},
+            {17,  "18"},
+            {18,  "19"},
+            {19,  "20"},
+            {20,  "21"},
+            {21,  "22"},
+            {22,  "23"},
+            {23,  "24"},
+            {24,  "25"},
+            {25,  "26"},
+            {26,  "27"},
+            {27,  "28"},
+            {28,  "29"},
+            {29,  "30"},
+            {30,  "31"},
+            {31,  "32"},
+            {32,  "33"},
+            {33,  "34"},
+            {34,  "35"},
+            {35,  "36"},
+            {36,  "0" },
+        };
+        [Command("ルーレット")]
+        public async Task ルーレット()
+        {
+            Random random = new Random();
+            int randomRule = random.Next(1);
+            int randomStage = random.Next(37);
+            string Messages = "\n ・**" + rule[randomRule].ToString() + "結果:\n" + stage[randomStage].ToString() + "**\n";
+            await ReplyAsync(Messages);
+        }
+    }
+}
